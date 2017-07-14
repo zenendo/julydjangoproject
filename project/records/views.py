f31e89731474cb1b18868b9b4a680078fa06e581
Zen Endo <zenendo08@gmail.com>
2017-07-13 21:14:19 -0400

    july13th

--- project/records/views.py
+++ project/records/views.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.list import ListView
+
+from .models import Post
+
+class PostListView(ListView):
+
+	model = Post
